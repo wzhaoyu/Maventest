bd4b64c301da0fc58d8888659aff68fc50806ae3
wzhaoyu <wzy564407388@gmail.com>
2021-12-30 15:13:38 +0800

    hot-fix first commit

--- src/main/java/com/wzy/git/GitTest.java
+++ src/main/java/com/wzy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello world!");
         System.out.println("hello world2!!!");
         System.out.println("hello world333!!!");
+        System.out.println("hello world4");
     }
 }
