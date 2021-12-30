088ac5c511cbdb22aae15aa8848b737d559689a5
wzhaoyu <wzy564407388@gmail.com>
2021-12-30 15:05:32 +0800

    idea first commit

---
+++ src/main/java/com/wzy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzy.git;
+
+/**
+ * @author Zhaoyu Wang
+ * @date 2021/12/30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
